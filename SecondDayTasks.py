a2a483fa6b9f8025d5a0ded827de05b947794e69
Nitish kumar <ku.nitish09@gmail.com>
2020-10-04 13:47:06 +0530

    Added Scripts for String operartions
    Indexing using String
    How to print the particular character from certain text
    Print the length of Character:

--- SecondDayTasks.py
+++ SecondDayTasks.py
@@ -5,3 +5,33 @@ print('30 days 30 hour challenge')
 # Assigning String to Variable
 Hours = "thirty"
 print(Hours)
+
+# Indexing using String
+Days = "Thirty days"
+print(Days[0])
+
+# How to print the particular character from certain text
+Challenge = "I will win"
+print(Challenge[2:6])
+print(len(Challenge))
+
+print(Challenge.lower())
+
+a ="30 Days"
+b ="30 hours"
+c = a +" " +b
+print(c)
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+y=text.capitalize()
+print(y)
+z=text.find("days")
+print(z)
+text1="Welcometopython"
+a=text1.isalpha()
+print(a)
+text2="Welcome2python"
+b=text2.isalnum()
+print(b)
